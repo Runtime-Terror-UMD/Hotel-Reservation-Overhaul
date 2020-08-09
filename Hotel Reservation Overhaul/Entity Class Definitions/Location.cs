368ce16e1da3825b7b8186be388fa2d00a465c83
Mariam Bitar <mbit@umich.edu>
2020-08-09 15:27:45 -0400

    added entity classes

---
+++ Hotel Reservation Overhaul/Entity Class Definitions/Location.cs
@@ -0,0 +1,10 @@
+﻿class Location
+{
+    private int locationID { get; set; }
+    private string name { get; set; }
+    Location(string name_)
+    {
+        //this.locationID = ?? from database
+        this.name = name_;
+    }
+}
