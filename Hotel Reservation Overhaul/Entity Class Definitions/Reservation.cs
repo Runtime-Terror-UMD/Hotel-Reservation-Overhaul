9435604a6216e7aa4354c2ff1aa7754713cc3b2b
Lynsey Nguyen <lynseyn@umich.edu>
2020-08-16 16:51:40 -0400

    saving the checkin changes not finished

--- Hotel Reservation Overhaul/Entity Class Definitions/Reservation.cs
+++ Hotel Reservation Overhaul/Entity Class Definitions/Reservation.cs
@@ -191,4 +191,43 @@ public class Reservation
         return confirmationID;
     }
 
+    // DESCRIPTION: Adds request to dbo.waitlist
+    public bool addToWaitlist(int wlUserID, int wlLocationID, DateTime wlStartDate, DateTime wlEndDate, int wlNumGuests, string combinedString)
+    {
+        DBConnect addToWLConn = new DBConnect();
+        MySqlCommand addToWL = new MySqlCommand(@"INSERT INTO `dbo`.`waitlist`(`customerID`,`locationID`,`startDate`,`endDate`,`numGuests`,`packages`)
+                                                  VALUES(@userID, @locationID, @startDate, @endDate, @numGuests,@packages)");
+        addToWL.Parameters.Add("@locationID", MySqlDbType.Int32).Value = wlLocationID;
+        addToWL.Parameters.Add("@userID", MySqlDbType.Int32, 10).Value = wlUserID;
+        addToWL.Parameters.Add("@startDate", MySqlDbType.Date).Value = wlStartDate;
+        addToWL.Parameters.Add("@endDate", MySqlDbType.Date).Value = wlEndDate;
+        addToWL.Parameters.Add("@numGuests", MySqlDbType.Int32).Value = wlNumGuests;
+        addToWL.Parameters.Add("@packages", MySqlDbType.VarChar, 45).Value = combinedString;
+        if (addToWLConn.NonQuery(addToWL) > 0)
+        {
+
+            LoggedActivity logNewReservation = new LoggedActivity();
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    //  DESCRIPTION: 
+    public bool checkOut(DateTime currentDate)
+    {
+        Utilities getInfo = new Utilities();
+        if(endDate != currentDate)
+        {
+            this.endDate = currentDate;
+            double pricePerNight = getInfo.getPricePerNight(locationID, roomNum);
+            totalPrice = pricePerNight * 
+        }
+    }
+    public bool checkIn()
+    {
+
+    }
 }
