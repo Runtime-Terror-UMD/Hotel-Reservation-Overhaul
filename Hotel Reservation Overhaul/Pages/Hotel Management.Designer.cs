57dbd8ef3a561be1fbdcfc2bb114c1dc3e6296cc
Lynsey Nguyen <lynseyn@umich.edu>
2020-08-16 15:31:19 -0400

    added passing date across pages

--- Hotel Reservation Overhaul/Pages/Hotel Management.Designer.cs
+++ Hotel Reservation Overhaul/Pages/Hotel Management.Designer.cs
@@ -134,8 +134,8 @@
             this.lblError.Location = new System.Drawing.Point(134, 288);
             this.lblError.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblError.Name = "lblError";
-            this.lblError.Size = new System.Drawing.Size(766, 31);
-            this.lblError.TabIndex = 12;
+            this.lblError.Size = new System.Drawing.Size(511, 20);
+            this.lblError.TabIndex = 0;
             this.lblError.Text = "Error:";
             this.lblError.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.lblError.Visible = false;
@@ -356,6 +356,17 @@
             this.lblFileStatus.Size = new System.Drawing.Size(600, 31);
             this.lblFileStatus.TabIndex = 21;
             this.lblFileStatus.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnTime
+            // 
+            this.btnTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTime.Location = new System.Drawing.Point(450, 120);
+            this.btnTime.Name = "btnTime";
+            this.btnTime.Size = new System.Drawing.Size(150, 50);
+            this.btnTime.TabIndex = 6;
+            this.btnTime.Text = "Go to Next Day";
+            this.btnTime.UseVisualStyleBackColor = true;
+            this.btnTime.Click += new System.EventHandler(this.btnTime_Click);
             // 
             // locationTableAdapter
             // 
