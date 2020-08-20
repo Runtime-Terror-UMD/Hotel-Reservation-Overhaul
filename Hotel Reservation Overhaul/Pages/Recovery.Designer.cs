316dfcc865b282d821ba0ca8f01494414449b025
Lynsey Nguyen <lynseyn@umich.edu>
2020-08-19 20:39:11 -0400

    adjusted field size of recovery screen so password cannot be longer than 45 char

--- Hotel Reservation Overhaul/Pages/Recovery.Designer.cs
+++ Hotel Reservation Overhaul/Pages/Recovery.Designer.cs
@@ -47,10 +47,9 @@
             // btnRecover
             // 
             this.btnRecover.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRecover.Location = new System.Drawing.Point(488, 612);
-            this.btnRecover.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnRecover.Location = new System.Drawing.Point(325, 398);
             this.btnRecover.Name = "btnRecover";
-            this.btnRecover.Size = new System.Drawing.Size(225, 46);
+            this.btnRecover.Size = new System.Drawing.Size(150, 30);
             this.btnRecover.TabIndex = 4;
             this.btnRecover.Text = "Reset Password";
             this.btnRecover.UseVisualStyleBackColor = true;
@@ -59,10 +58,10 @@
             // txtSAns
             // 
             this.txtSAns.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSAns.Location = new System.Drawing.Point(565, 441);
-            this.txtSAns.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtSAns.Location = new System.Drawing.Point(377, 287);
+            this.txtSAns.MaxLength = 250;
             this.txtSAns.Name = "txtSAns";
-            this.txtSAns.Size = new System.Drawing.Size(223, 28);
+            this.txtSAns.Size = new System.Drawing.Size(150, 21);
             this.txtSAns.TabIndex = 2;
             this.txtSAns.UseSystemPasswordChar = true;
             this.txtSAns.Visible = false;
@@ -70,10 +69,9 @@
             // lblSAns
             // 
             this.lblSAns.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSAns.Location = new System.Drawing.Point(358, 441);
-            this.lblSAns.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSAns.Location = new System.Drawing.Point(239, 287);
             this.lblSAns.Name = "lblSAns";
-            this.lblSAns.Size = new System.Drawing.Size(192, 31);
+            this.lblSAns.Size = new System.Drawing.Size(128, 20);
             this.lblSAns.TabIndex = 21;
             this.lblSAns.Text = "Secret Answer:";
             this.lblSAns.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -82,10 +80,9 @@
             // lblSQuest
             // 
             this.lblSQuest.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSQuest.Location = new System.Drawing.Point(334, 380);
-            this.lblSQuest.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSQuest.Location = new System.Drawing.Point(223, 247);
             this.lblSQuest.Name = "lblSQuest";
-            this.lblSQuest.Size = new System.Drawing.Size(216, 31);
+            this.lblSQuest.Size = new System.Drawing.Size(144, 20);
             this.lblSQuest.TabIndex = 22;
             this.lblSQuest.Text = "Secret Question:";
             this.lblSQuest.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -95,10 +92,9 @@
             // 
             this.lblError.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblError.ForeColor = System.Drawing.Color.Red;
-            this.lblError.Location = new System.Drawing.Point(300, 692);
-            this.lblError.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblError.Location = new System.Drawing.Point(200, 450);
             this.lblError.Name = "lblError";
-            this.lblError.Size = new System.Drawing.Size(600, 62);
+            this.lblError.Size = new System.Drawing.Size(400, 40);
             this.lblError.TabIndex = 23;
             this.lblError.Text = "Error:";
             this.lblError.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -107,10 +103,9 @@
             // lblDescribe
             // 
             this.lblDescribe.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDescribe.Location = new System.Drawing.Point(300, 215);
-            this.lblDescribe.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDescribe.Location = new System.Drawing.Point(200, 140);
             this.lblDescribe.Name = "lblDescribe";
-            this.lblDescribe.Size = new System.Drawing.Size(600, 62);
+            this.lblDescribe.Size = new System.Drawing.Size(400, 40);
             this.lblDescribe.TabIndex = 24;
             this.lblDescribe.Text = "Password Reset: Enter username and answer your secret question, and the desired n" +
     "ew password.";
@@ -119,19 +114,18 @@
             // txtUser
             // 
             this.txtUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUser.Location = new System.Drawing.Point(565, 318);
-            this.txtUser.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtUser.Location = new System.Drawing.Point(377, 207);
+            this.txtUser.MaxLength = 45;
             this.txtUser.Name = "txtUser";
-            this.txtUser.Size = new System.Drawing.Size(223, 28);
+            this.txtUser.Size = new System.Drawing.Size(150, 21);
             this.txtUser.TabIndex = 1;
             // 
             // lblUsername
             // 
             this.lblUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUsername.Location = new System.Drawing.Point(400, 318);
-            this.lblUsername.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblUsername.Location = new System.Drawing.Point(267, 207);
             this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(150, 31);
+            this.lblUsername.Size = new System.Drawing.Size(100, 20);
             this.lblUsername.TabIndex = 25;
             this.lblUsername.Text = "Username:";
             this.lblUsername.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -139,10 +133,9 @@
             // lblTitle
             // 
             this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitle.Location = new System.Drawing.Point(300, 154);
-            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitle.Location = new System.Drawing.Point(200, 100);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(600, 62);
+            this.lblTitle.Size = new System.Drawing.Size(400, 40);
             this.lblTitle.TabIndex = 26;
             this.lblTitle.Text = "Hotel Reservation System";
             this.lblTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -150,20 +143,19 @@
             // lblQuest
             // 
             this.lblQuest.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblQuest.Location = new System.Drawing.Point(565, 380);
-            this.lblQuest.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblQuest.Location = new System.Drawing.Point(377, 247);
             this.lblQuest.Name = "lblQuest";
-            this.lblQuest.Size = new System.Drawing.Size(420, 32);
+            this.lblQuest.Size = new System.Drawing.Size(280, 21);
             this.lblQuest.TabIndex = 33;
             this.lblQuest.Visible = false;
             // 
             // txtPassword
             // 
             this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPassword.Location = new System.Drawing.Point(565, 503);
-            this.txtPassword.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtPassword.Location = new System.Drawing.Point(377, 327);
+            this.txtPassword.MaxLength = 45;
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(223, 28);
+            this.txtPassword.Size = new System.Drawing.Size(150, 21);
             this.txtPassword.TabIndex = 3;
             this.txtPassword.UseSystemPasswordChar = true;
             this.txtPassword.Visible = false;
@@ -171,10 +163,9 @@
             // lblPassword
             // 
             this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPassword.Location = new System.Drawing.Point(364, 503);
-            this.lblPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPassword.Location = new System.Drawing.Point(243, 327);
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(186, 31);
+            this.lblPassword.Size = new System.Drawing.Size(124, 20);
             this.lblPassword.TabIndex = 35;
             this.lblPassword.Text = "New Password:";
             this.lblPassword.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -183,10 +174,9 @@
             // btnVerifyAcct
             // 
             this.btnVerifyAcct.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnVerifyAcct.Location = new System.Drawing.Point(488, 612);
-            this.btnVerifyAcct.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnVerifyAcct.Location = new System.Drawing.Point(325, 398);
             this.btnVerifyAcct.Name = "btnVerifyAcct";
-            this.btnVerifyAcct.Size = new System.Drawing.Size(225, 46);
+            this.btnVerifyAcct.Size = new System.Drawing.Size(150, 30);
             this.btnVerifyAcct.TabIndex = 36;
             this.btnVerifyAcct.Text = "Verify Account";
             this.btnVerifyAcct.UseVisualStyleBackColor = true;
@@ -195,10 +185,9 @@
             // btnReturn
             // 
             this.btnReturn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnReturn.Location = new System.Drawing.Point(13, 30);
-            this.btnReturn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnReturn.Location = new System.Drawing.Point(9, 19);
             this.btnReturn.Name = "btnReturn";
-            this.btnReturn.Size = new System.Drawing.Size(225, 46);
+            this.btnReturn.Size = new System.Drawing.Size(150, 30);
             this.btnReturn.TabIndex = 37;
             this.btnReturn.Text = "Return to Login";
             this.btnReturn.UseVisualStyleBackColor = true;
@@ -207,9 +196,9 @@
             // Recovery
             // 
             this.AcceptButton = this.btnVerifyAcct;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1176, 863);
+            this.ClientSize = new System.Drawing.Size(784, 561);
             this.Controls.Add(this.btnReturn);
             this.Controls.Add(this.btnVerifyAcct);
             this.Controls.Add(this.txtPassword);
@@ -224,7 +213,6 @@
             this.Controls.Add(this.txtUser);
             this.Controls.Add(this.lblUsername);
             this.Controls.Add(this.lblTitle);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Recovery";
             this.Text = "Hotel Reservation: Recovery";
             this.ResumeLayout(false);
