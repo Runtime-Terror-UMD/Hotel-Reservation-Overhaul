7310ef10fbbdec7b82e2a2e5f63abfc37644bf7f
jsulumich <68516474+jsulumich@users.noreply.github.com>
2020-08-20 23:48:55 -0400

    occupancy report

--- Hotel Reservation Overhaul/Pages/OccupancySummary.Designer.cs
+++ Hotel Reservation Overhaul/Pages/OccupancySummary.Designer.cs
@@ -46,10 +46,9 @@
             // 
             this.lblStart.AutoSize = true;
             this.lblStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.lblStart.Location = new System.Drawing.Point(32, 28);
-            this.lblStart.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblStart.Location = new System.Drawing.Point(48, 43);
             this.lblStart.Name = "lblStart";
-            this.lblStart.Size = new System.Drawing.Size(78, 17);
+            this.lblStart.Size = new System.Drawing.Size(109, 25);
             this.lblStart.TabIndex = 1;
             this.lblStart.Text = "From Date:";
             // 
@@ -57,10 +56,9 @@
             // 
             this.lblEnd.AutoSize = true;
             this.lblEnd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.lblEnd.Location = new System.Drawing.Point(32, 68);
-            this.lblEnd.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblEnd.Location = new System.Drawing.Point(48, 105);
             this.lblEnd.Name = "lblEnd";
-            this.lblEnd.Size = new System.Drawing.Size(63, 17);
+            this.lblEnd.Size = new System.Drawing.Size(88, 25);
             this.lblEnd.TabIndex = 2;
             this.lblEnd.Text = "To Date:";
             // 
@@ -69,9 +67,10 @@
             this.lblFromDate.BackColor = System.Drawing.SystemColors.Window;
             this.lblFromDate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.lblFromDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblFromDate.Location = new System.Drawing.Point(132, 28);
+            this.lblFromDate.Location = new System.Drawing.Point(198, 43);
+            this.lblFromDate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblFromDate.Name = "lblFromDate";
-            this.lblFromDate.Size = new System.Drawing.Size(150, 20);
+            this.lblFromDate.Size = new System.Drawing.Size(224, 30);
             this.lblFromDate.TabIndex = 3;
             this.lblFromDate.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
@@ -80,9 +79,10 @@
             this.lblToDate.BackColor = System.Drawing.SystemColors.Window;
             this.lblToDate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.lblToDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblToDate.Location = new System.Drawing.Point(132, 68);
+            this.lblToDate.Location = new System.Drawing.Point(198, 105);
+            this.lblToDate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblToDate.Name = "lblToDate";
-            this.lblToDate.Size = new System.Drawing.Size(150, 20);
+            this.lblToDate.Size = new System.Drawing.Size(224, 30);
             this.lblToDate.TabIndex = 4;
             this.lblToDate.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
@@ -90,10 +90,9 @@
             // 
             this.lblOccupied.AutoSize = true;
             this.lblOccupied.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.lblOccupied.Location = new System.Drawing.Point(96, 143);
-            this.lblOccupied.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblOccupied.Location = new System.Drawing.Point(144, 220);
             this.lblOccupied.Name = "lblOccupied";
-            this.lblOccupied.Size = new System.Drawing.Size(152, 17);
+            this.lblOccupied.Size = new System.Drawing.Size(212, 25);
             this.lblOccupied.TabIndex = 5;
             this.lblOccupied.Text = "% of Occupied Rooms:";
             // 
@@ -102,9 +101,10 @@
             this.txtOccupied.BackColor = System.Drawing.SystemColors.Window;
             this.txtOccupied.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtOccupied.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtOccupied.Location = new System.Drawing.Point(260, 143);
+            this.txtOccupied.Location = new System.Drawing.Point(390, 220);
+            this.txtOccupied.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.txtOccupied.Name = "txtOccupied";
-            this.txtOccupied.Size = new System.Drawing.Size(150, 20);
+            this.txtOccupied.Size = new System.Drawing.Size(224, 30);
             this.txtOccupied.TabIndex = 6;
             this.txtOccupied.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
@@ -112,21 +112,21 @@
             // 
             this.lblUnOccupied.AutoSize = true;
             this.lblUnOccupied.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.lblUnOccupied.Location = new System.Drawing.Point(78, 179);
-            this.lblUnOccupied.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblUnOccupied.Location = new System.Drawing.Point(117, 275);
             this.lblUnOccupied.Name = "lblUnOccupied";
-            this.lblUnOccupied.Size = new System.Drawing.Size(170, 17);
+            this.lblUnOccupied.Size = new System.Drawing.Size(232, 25);
             this.lblUnOccupied.TabIndex = 7;
-            this.lblUnOccupied.Text = "% of UnOccupied Rooms:";
+            this.lblUnOccupied.Text = "% of Unoccupied Rooms:";
             // 
             // txtUnoccupied
             // 
             this.txtUnoccupied.BackColor = System.Drawing.SystemColors.Window;
             this.txtUnoccupied.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtUnoccupied.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUnoccupied.Location = new System.Drawing.Point(260, 179);
+            this.txtUnoccupied.Location = new System.Drawing.Point(390, 275);
+            this.txtUnoccupied.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.txtUnoccupied.Name = "txtUnoccupied";
-            this.txtUnoccupied.Size = new System.Drawing.Size(150, 20);
+            this.txtUnoccupied.Size = new System.Drawing.Size(224, 30);
             this.txtUnoccupied.TabIndex = 8;
             this.txtUnoccupied.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
@@ -134,10 +134,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label2.Location = new System.Drawing.Point(32, 218);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(48, 335);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(216, 17);
+            this.label2.Size = new System.Drawing.Size(299, 25);
             this.label2.TabIndex = 9;
             this.label2.Text = "% of Rooms Under Maintenance:";
             // 
@@ -146,9 +145,10 @@
             this.txtMaintenance.BackColor = System.Drawing.SystemColors.Window;
             this.txtMaintenance.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtMaintenance.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMaintenance.Location = new System.Drawing.Point(260, 218);
+            this.txtMaintenance.Location = new System.Drawing.Point(390, 335);
+            this.txtMaintenance.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.txtMaintenance.Name = "txtMaintenance";
-            this.txtMaintenance.Size = new System.Drawing.Size(150, 20);
+            this.txtMaintenance.Size = new System.Drawing.Size(224, 30);
             this.txtMaintenance.TabIndex = 10;
             this.txtMaintenance.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
@@ -156,10 +156,9 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label4.Location = new System.Drawing.Point(143, 253);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(214, 389);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(105, 17);
+            this.label4.Size = new System.Drawing.Size(145, 25);
             this.label4.TabIndex = 11;
             this.label4.Text = "Total Revenue:";
             // 
@@ -168,17 +167,18 @@
             this.txtRevenue.BackColor = System.Drawing.SystemColors.Window;
             this.txtRevenue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtRevenue.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtRevenue.Location = new System.Drawing.Point(260, 253);
+            this.txtRevenue.Location = new System.Drawing.Point(390, 389);
+            this.txtRevenue.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.txtRevenue.Name = "txtRevenue";
-            this.txtRevenue.Size = new System.Drawing.Size(150, 20);
+            this.txtRevenue.Size = new System.Drawing.Size(224, 30);
             this.txtRevenue.TabIndex = 12;
             this.txtRevenue.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // OccupancySummary
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(533, 338);
+            this.ClientSize = new System.Drawing.Size(800, 520);
             this.Controls.Add(this.txtRevenue);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.txtMaintenance);
@@ -191,6 +191,7 @@
             this.Controls.Add(this.lblFromDate);
             this.Controls.Add(this.lblEnd);
             this.Controls.Add(this.lblStart);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "OccupancySummary";
             this.Text = "Hotel Reservation: Occupancy Summary";
             this.ResumeLayout(false);
